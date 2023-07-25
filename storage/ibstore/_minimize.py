e0751ce93eccfddb348d6a6913de4d59bfb875b6
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-07-25 15:58:32 -0500

    Better parallelize minimization

--- storage/ibstore/_minimize.py
+++ storage/ibstore/_minimize.py
@@ -22,44 +22,62 @@ FORCE_FIELD = ForceField("openff_unconstrained-2.0.0.offxml")
 
 def _minimize_blob(input: dict[str, dict[str, Union[str, numpy.ndarray]]]):
     returned = defaultdict(list)
-    tmp = list()
+    inputs = list()
 
     with _toolkit_registry_manager(OpenEyeToolkitWrapper()):
-        for inchi_key in tqdm(input, desc="Creating systems"):
-            molecule = Molecule.from_inchi(inchi_key)
-
-            system = FORCE_FIELD.create_openmm_system(molecule.to_topology())
-
+        for inchi_key in input:
             for row in input[inchi_key]:
-                tmp.append(
-                    tuple(
-                        (
-                            inchi_key,
-                            row["qcarchive_id"],
-                            row["coordinates"],
-                            system,
-                        )
+                inputs.append(
+                    MinimizationInput(
+                        inchi_key=inchi_key,
+                        qcarchive_id=row["qcarchive_id"],
+                        coordinates=row["coordinates"],
                     )
                 )
 
         with Pool(processes=N_PROCESSES) as pool:
-            for inchi_key, result in pool.starmap(
-                _run_openmm, tqdm(tmp, desc="Minimizing systems")
+            for result in tqdm(
+                pool.imap(
+                    _run_openmm,
+                    inputs,
+                ),
+                desc="Building and minimizing systems",
+                total=len(inputs),
             ):
-                returned[inchi_key].append(result)
+                returned[result.inchi_key].append(result)
 
     return returned
 
 
-class MinimizedResult(ImmutableModel):
+class MinimizationInput(ImmutableModel):
+    inchi_key: str = Field(..., description="The InChI key of the molecule")
+    qcarchive_id: str = Field(
+        ..., description="The ID of the molecule in the QCArchive"
+    )
+    coordinates: Array = Field(
+        ...,
+        description="The coordinates [Angstrom] of this conformer with shape=(n_atoms, 3).",
+    )
+
+
+class MinimizationResult(ImmutableModel):
+    inchi_key: str = Field(..., description="The InChI key of the molecule")
     qcarchive_id: str
     coordinates: Array
     energy: float = Field(..., description="Minimized energy in kcal/mol")
 
 
 def _run_openmm(
-    inchi_key: str, qcarchive_id: str, positions: numpy.ndarray, system: openmm.System
-):
+    input: MinimizationInput,
+) -> MinimizationResult:
+    inchi_key: str = input.inchi_key
+    qcarchive_id: str = input.qcarchive_id
+    positions: numpy.ndarray = input.coordinates
+
+    molecule = Molecule.from_inchi(inchi_key)
+
+    system = FORCE_FIELD.create_openmm_system(molecule.to_topology())
+
     context = openmm.Context(
         system,
         openmm.VerletIntegrator(0.1 * openmm.unit.femtoseconds),
@@ -69,7 +87,8 @@ def _run_openmm(
     context.setPositions(positions * openmm.unit.angstrom)
     openmm.LocalEnergyMinimizer.minimize(context, 5.0e-9, 1500)
 
-    return inchi_key, MinimizedResult(
+    return MinimizationResult(
+        inchi_key=inchi_key,
         qcarchive_id=qcarchive_id,
         coordinates=context.getState(getPositions=True).getPositions(asNumpy=True),
         energy=context.getState(
