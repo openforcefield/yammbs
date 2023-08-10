4d5c93cf660e894271affa29a5f94e2d4cf7ec16
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-08-10 14:10:02 -0500

    Fix tests

--- ibstore/_tests/unit_tests/test_store.py
+++ ibstore/_tests/unit_tests/test_store.py
@@ -1,21 +1,32 @@
 import tempfile
 
+import pytest
 from openff.utilities import get_data_file_path
 
 from ibstore._store import MoleculeStore
+from ibstore.exceptions import DatabaseExistsError
 
 
 def test_from_qcsubmit(small_collection):
+    store = MoleculeStore.from_qcsubmit_collection(
+        small_collection,
+        "foo.sqlite",
+    )
+
+    # Sanity check molecule deduplication
+    assert len(store.get_smiles()) == len({*store.get_smiles()})
+
+    # Ensure a new object can be created from the same database
+    assert len(MoleculeStore("foo.sqlite")) == len(store)
+
+
+def test_do_not_overwrite(small_collection):
     with tempfile.NamedTemporaryFile(suffix=".sqlite") as file:
-        store = MoleculeStore.from_qcsubmit_collection(
-            small_collection,
-            file.name,
-        )
-
-        # Sanity check molecule deduplication
-        assert len(store.get_smiles()) == len({*store.get_smiles()})
-
-        assert len(MoleculeStore(file.name)) == len(store)
+        with pytest.raises(DatabaseExistsError, match="already exists."):
+            MoleculeStore.from_qcsubmit_collection(
+                small_collection,
+                file.name,
+            )
 
 
 def test_load_existing_databse():
