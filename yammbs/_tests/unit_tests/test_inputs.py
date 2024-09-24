2e387faf04bb86086fe5e851113fda6f46edcdbe
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-09-24 10:11:38 -0500

    Fix test

--- yammbs/_tests/unit_tests/test_inputs.py
+++ yammbs/_tests/unit_tests/test_inputs.py
@@ -26,7 +26,7 @@ class TestQCArchiveDataset:
 class TestSerialization:
     def test_json_roundtrip(self):
         dataset = QCArchiveDataset.from_json(
-            get_data_file_path("_tests/data/qcsubmit/01-processed-qm-ch.json", "yammbs"),
+            get_data_file_path("_tests/data/yammbs/01-processed-qm-ch.json", "yammbs"),
         )
 
         roundtripped = QCArchiveDataset.parse_raw(dataset.json())
