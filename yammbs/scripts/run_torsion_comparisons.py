e6b9bc9964b759039bde5f7d1c181790e7e20d9b
Finlay Clark <finlay.clark@newcastle.ac.uk>
2025-03-25 15:58:30 +0000

    Improve plotting style and add plots of rms values

--- yammbs/scripts/run_torsion_comparisons.py
+++ yammbs/scripts/run_torsion_comparisons.py
@@ -9,14 +9,23 @@ from yammbs.torsion import TorsionStore
 from yammbs.torsion.inputs import QCArchiveTorsionDataset
 from yammbs.torsion.outputs import MetricCollection
 
+pyplot.style.use("ggplot")
+
 
 @click.command()
 @click.option(
-    "--force-fields",
-    "-f",
+    "--base-force-fields",
+    "-bf",
     multiple=True,
-    default=["openff-1.0.0", "openff-2.0.0", "openff-2.1.0", "openff-2.2.0", "openff-2.2.1"],
+    default=["openff-1.0.0", "openff-2.0.0", "openff-2.1.0", "openff-2.2.1"],
     help="List of force fields to use for optimization.",
+)
+@click.option(
+    "--extra-force-fields",
+    "-ef",
+    multiple=True,
+    default=[],
+    help="Extra (local) force fields to use for optimization.",
 )
 @click.option(
     "--qcarchive-torsion-data",
@@ -49,7 +58,8 @@ from yammbs.torsion.outputs import MetricCollection
     help="Directory to save the generated plots.",
 )
 def main(
-    force_fields: list[str],
+    base_force_fields: list[str],
+    extra_force_fields: list[str],
     qcarchive_torsion_data: str,
     database_file: str,
     output_metrics: str,
@@ -59,6 +69,8 @@ def main(
     """
     Run torsion drive comparisons using specified force fields and input data.
     """
+    force_fields = base_force_fields + extra_force_fields
+
     dataset = QCArchiveTorsionDataset.model_validate_json(open(qcarchive_torsion_data).read())
 
     if pathlib.Path(database_file).exists():
@@ -80,6 +92,13 @@ def main(
         with open(output_metrics, "w") as f:
             f.write(store.get_metrics().model_dump_json())
 
+    # Plot!
+    plot_torsions(plot_dir, force_fields, store)
+    plot_cdfs(force_fields, output_metrics, plot_dir)
+    plot_rms_stats(output_metrics, plot_dir)
+
+
+def plot_torsions(plot_dir: str, force_fields: list[str], store: TorsionStore) -> None:
     fig, axes = pyplot.subplots(5, 4, figsize=(20, 20))
 
     for molecule_id, axis in zip(store.get_molecule_ids(), axes.flatten()):
@@ -92,8 +111,12 @@ def main(
         # Make a new dict to avoid in-place modification while iterating
         qm = {key: _qm[key] - _qm[qm_minimum_index] for key in _qm}
 
+        # Assume a default grid spacing of 15 degrees (BespokeFit default)
+        angles = np.arange(-165, 195, 15)
+        assert len(angles) == len(qm), "QM data and angles should match in length"
+
         axis.plot(
-            qm.keys(),
+            angles,
             qm.values(),
             "k.-",
             label=f"QM {molecule_id}",
@@ -105,26 +128,32 @@ def main(
                 continue
 
             axis.plot(
-                mm.keys(),
+                angles,
                 [val - mm[qm_minimum_index] for val in mm.values()],
                 "o--",
                 label=force_field,
             )
 
         axis.legend(loc=0)
-        axis.grid(axis="both")
+
+        # Label the axes
+        axis.set_ylabel(r"Energy / kcal mol$^{-1}$")
+        axis.set_xlabel("Torsion angle / degrees")
 
     fig.savefig(f"{plot_dir}/random.png")
 
-    plot_cdf(force_fields, output_metrics, plot_dir)
 
-
-def plot_cdf(force_fields: list[str], metrics_file: str, plot_dir: str):
+def plot_cdfs(force_fields: list[str], metrics_file: str, plot_dir: str):
     metrics = MetricCollection.parse_file(metrics_file)
 
     x_ranges = {
-        "rmsd": (0, 0.18),
+        "rmsd": (0, 0.14),
         "rmse": (-0.3, 5),
+    }
+
+    units = {
+        "rmsd": r"$\mathrm{\AA}$",
+        "rmse": r"kcal mol$^{-1}$",
     }
 
     rmsds = {
@@ -170,7 +199,7 @@ def plot_cdf(force_fields: list[str], metrics_file: str, plot_dir: str):
                     label=force_field,
                 )
 
-                axis.set_xlabel(key)
+                axis.set_xlabel(key.upper() + " / " + units[key])
                 axis.set_ylabel("CDF")
 
                 axis.set_xlim(x_ranges[key])
@@ -181,10 +210,53 @@ def plot_cdf(force_fields: list[str], metrics_file: str, plot_dir: str):
         figure.savefig(f"{plot_dir}/{key}.png", dpi=300)
 
 
+def get_rms(array: np.ndarray) -> float:
+    """
+    Calculate the root mean square of an array.
+    """
+    return np.sqrt(np.mean(array**2))
+
+
+def plot_rms_stats(
+    metrics_file: str,
+    plot_dir: str,
+) -> None:
+    metrics = MetricCollection.parse_file(metrics_file)
+
+    units = {
+        "rmsd": r"$\mathrm{\AA}$",
+        "rmse": r"kcal mol$^{-1}$",
+    }
+
+    rms_rmses = {
+        force_field: get_rms(np.array([val.rmse for val in metrics.metrics[force_field].values()]))
+        for force_field in metrics.metrics.keys()
+    }
+
+    rms_rmsds = {
+        force_field: get_rms(np.array([val.rmsd for val in metrics.metrics[force_field].values()]))
+        for force_field in metrics.metrics.keys()
+    }
+
+    # Plot RMS values
+    for key, data in zip(["rmsd", "rmse"], [rms_rmsds, rms_rmses]):
+        figure, axis = pyplot.subplots()
+
+        # Use different colors for each bar - the same as for the CDFs
+        axis.bar(data.keys(), data.values(), color=pyplot.cm.tab10.colors)
+        axis.set_ylabel(key.upper() + " / " + units[key])
+
+        # Set x-ticks to be vertical
+        pyplot.xticks(rotation=90)
+
+        # Save the figure
+        figure.tight_layout()
+        figure.savefig(f"{plot_dir}/{key}_rms.png", dpi=300, bbox_inches="tight")
+
+
 if __name__ == "__main__":
     # This setup is necessary for reasons that confused me - both setting it up in the __main__ block and calling
     # freeze_support(). This is probably not necessary after MoleculeStore.optimize_mm() is called, so you can load up
     # the same database for later analysis once the MM conformers are stored
-    pyplot.style.use("ggplot")
     freeze_support()
     main()
