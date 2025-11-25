2195f50dd4890fa97c9da32276a5215c916946b4
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-11-25 16:40:31 -0600

    Add bond and dihedral functions, improve conversion

--- yammbs/_mdanalysis.py
+++ yammbs/_mdanalysis.py
@@ -1,8 +1,36 @@
+import functools
+
 import MDAnalysis
 from openff.toolkit import Molecule
 
+
 def from_openff(molecule: Molecule) -> MDAnalysis.Universe:
+    return from_openff_mapped(
+        molecule,
+        molecule.to_smiles(mapped=True),
+    )
+
+
+@functools.lru_cache
+def from_openff_mapped(molecule: Molecule, mapped_smiles: str) -> MDAnalysis.Universe:
     return MDAnalysis.Universe(molecule.to_rdkit())
+
+
+def get_bonds(molecule: Molecule) -> dict[tuple[int, int], float]:
+    assert molecule.n_conformers == 1, "Need a conformer for coordinate analysis"
+
+    universe = from_openff(molecule)
+
+    result = dict()
+
+    for bond in molecule.bonds:
+        atom_indices = [bond.atom1_index, bond.atom2_index]
+        bond_coords = universe.coord.positions[atom_indices]
+        # Process angle using MDAnalysis
+        result[tuple(atom_indices)] = MDAnalysis.lib.distances.calc_bonds(*bond_coords)
+
+    return result
+
 
 def get_angles(molecule: Molecule) -> dict[tuple[int, int, int], float]:
     assert molecule.n_conformers == 1, "Need a conformer for coordinate analysis"
@@ -15,6 +43,49 @@ def get_angles(molecule: Molecule) -> dict[tuple[int, int, int], float]:
         atom_indices = [molecule.atom_index(atom) for atom in angle]
         angle_coords = universe.coord.positions[atom_indices]
         # TODO: would this be faster if we gave it all angles at once?
-        result[tuple(atom_indices)] =  MDAnalysis.lib.distances.calc_angles(*angle_coords)
+        result[tuple(atom_indices)] = MDAnalysis.lib.distances.calc_angles(*angle_coords)
+
+    return result
+
+
+def get_proper_torsions(molecule: Molecule) -> dict[tuple[int, int, int, int], float]:
+    assert molecule.n_conformers == 1, "Need a conformer for coordinate analysis"
+
+    universe = from_openff(molecule)
+
+    result = dict()
+
+    for dihedral in molecule.propers:
+        atom_indices = [molecule.atom_index(atom) for atom in dihedral]
+        dihedral_coords = universe.coord.positions[atom_indices]
+        result[tuple(atom_indices)] = MDAnalysis.lib.distances.calc_dihedrals(*dihedral_coords)
+
+    return result
+
+
+def get_improper_torsions(molecule: Molecule) -> dict[tuple[int, int, int, int], float]:
+    assert molecule.n_conformers == 1, "Need a conformer for coordinate analysis"
+
+    universe = from_openff(molecule)
+
+    result = dict()
+
+    for dihedral in molecule.smirnoff_impropers:
+        # central atom is SECOND
+        atom_indices = [molecule.atom_index(atom) for atom in dihedral]
+
+        # calc_dihedrals builds planes using i-j-k and j-k-l, and following that definition
+        # the central atom must be first (i)
+        dihedral_coords = universe.coord.positions[
+            [
+                atom_indices[1],
+                atom_indices[0],
+                atom_indices[2],
+                atom_indices[3],
+            ]
+        ]
+
+        # but don't re-order when bookkeeping
+        result[tuple(atom_indices)] = MDAnalysis.lib.distances.calc_dihedrals(*dihedral_coords)
 
     return result
