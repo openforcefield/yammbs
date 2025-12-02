09fe95cd28880791a023daba2c083db15e3cac24
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-12-02 14:10:18 -0600

    Start adding test structure

---
+++ yammbs/_tests/unit_tests/test_mdanalysis.py
@@ -0,0 +1,39 @@
+import pytest
+
+import numpy
+
+from yammbs._mdanalysis import from_openff
+from openff.toolkit.topology.molecule import Bond
+from openff.toolkit import Molecule
+from MDAnalysis.core.topologyobjects import TopologyGroup
+def assert_bonds_match(
+    openff_bonds: list[Bond],
+    mdanalysis_bonds: TopologyGroup,
+):
+    assert len(openff_bonds) == len(mdanalysis_bonds)
+
+    openff_array = numpy.sort(
+        numpy.array(
+            [[bond.atom1_index, bond.atom2_index] for bond in openff_bonds],
+            dtype=numpy.int32,
+        ),
+        axis=0,
+    )
+    mdanalysis_array = numpy.sort(
+        numpy.array(
+            [bond.indices for bond in mdanalysis_bonds], dtype=numpy.int32,
+        ),
+        axis=0,
+    )
+
+    numpy.testing.assert_allclose(openff_array, mdanalysis_array)
+
+
+
+@pytest.mark.parametrize("smiles", ["CCO", "c1ccccc1", "CC(=O)O", "C1CC1N"])
+def test_valences_match(smiles):
+    molecule = Molecule.from_smiles(smiles)
+
+    universe = from_openff(molecule)
+
+    assert_bonds_match(molecule.bonds, universe.bonds)
