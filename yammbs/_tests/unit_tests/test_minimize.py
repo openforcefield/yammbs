1fbfe6f865c13a6ca183013e347d2e4e754d0dc2
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-01-20 09:10:04 -0600

    Build test set ground-up

--- yammbs/_tests/unit_tests/test_minimize.py
+++ yammbs/_tests/unit_tests/test_minimize.py
@@ -1,5 +1,3 @@
-import platform
-
 import numpy
 import pytest
 from openff.toolkit import ForceField, Molecule, unit
@@ -7,7 +5,7 @@ from openff.toolkit import __version__ as __toolkit_version__
 
 from yammbs import MoleculeStore
 from yammbs._minimize import MinimizationInput, _run_openmm
-from yammbs.inputs import QCArchiveDataset
+from yammbs.inputs import QCArchiveDataset, QCArchiveMolecule
 
 
 @pytest.fixture
@@ -172,7 +170,7 @@ def test_plugin_not_needed_to_use_mainline_force_field(monkeypatch, ethane):
 
 
 @pytest.mark.timeout(60)
-def test_partially_minimized(tiny_cache, tmp_path):
+def test_partially_minimized(tmp_path):
     """Test that minimizing with one force field produces expected results.
 
     See https://github.com/mattwthompson/ib/pull/21#discussion_r1511804909
@@ -197,18 +195,24 @@ def test_partially_minimized(tiny_cache, tmp_path):
             get_n_mm_conformers(store, "openff-2.0.0"),
         )
 
-    # No need to minimize all 200 records twice ...
-    tinier_cache = QCArchiveDataset()
+    dataset = QCArchiveDataset(tag="test dataset")
 
-    # ... so slice out some really small molecules (< 7 heavy atoms)
-    # which should be 4 molecules for this dataset
-    for result in tiny_cache.qm_molecules:
-        molecule = Molecule.from_mapped_smiles(result.mapped_smiles)
-        if len([atom for atom in molecule.atoms if atom.atomic_number > 1]) < 7:
-            tinier_cache.qm_molecules.append(result)
+    for index, smiles in enumerate(["CCO", "CC(=O)CC"]):
+        molecule = Molecule.from_smiles(smiles)
+        molecule.generate_conformers(n_conformers=1)
+
+        dataset.qm_molecules.append(
+            QCArchiveMolecule(
+                id=index,
+                mapped_smiles=molecule.to_smiles(mapped=True),
+                coordinates=molecule.conformers[0].m_as("angstrom"),
+                qcarchive_id=index,
+                final_energy=0.0,
+            ),
+        )
 
     tinier_store = MoleculeStore.from_qcarchive_dataset(
-        tinier_cache,
+        dataset,
         database_name=(tmp_path / "tiny.sqlite").as_posix(),
     )
 
@@ -216,11 +220,11 @@ def test_partially_minimized(tiny_cache, tmp_path):
 
     tinier_store.optimize_mm(force_field="openff-1.0.0", n_processes=1)
 
-    assert get_n_results(tinier_store) == (4, 0)
+    assert get_n_results(tinier_store) == (2, 0)
 
     tinier_store.optimize_mm(force_field="openff-2.0.0", n_processes=1)
 
-    assert get_n_results(tinier_store) == (4, 4)
+    assert get_n_results(tinier_store) == (2, 2)
 
     assert tinier_store.software_provenance["yammbs"] == yammbs.__version__
     assert tinier_store.software_provenance["openff.toolkit"] == __toolkit_version__
