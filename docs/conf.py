c3d4e28ad61cdd3ae140595cbb258b4527f010bd
jaclark5 <jennifer.clark@openforcefield.org>
2026-07-08 14:53:58 -0400

    Add notebook scrolling

--- docs/conf.py
+++ docs/conf.py
@@ -221,6 +221,9 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# Custom CSS files for notebook output styling
+html_css_files = ["custom.css"]
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 html_sidebars = {
